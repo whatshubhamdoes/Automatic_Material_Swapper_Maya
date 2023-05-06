2a4eae222c82762d6b7992b68ed61a3c3eda363f
Shubham Prabhakar <shubhamprabhakar14@gmail.com>
2023-05-06 14:02:13 +0100

    .

--- atr_code.py
+++ atr_code.py
@@ -1,15 +1,16 @@
 import maya.cmds as cmds
+import maya._OpenMaya as OpenMaya
+import maya.OpenMayaUI as OpenMayaUI
 import maya.mel as mel
 import os
 import sys
-import maya._OpenMaya as OpenMaya
-import maya.OpenMayaUI as OpenMayaUI
 
 def convert_diffuse_material() :
     # get selected object and if not selected prompt user to select an object#
     sel = cmds.ls(sl=True)
     if not sel :
         cmds.confirmDialog(title='Error', message='Please select an object', button=['OK'], defaultButton='OK')
+        cmds.warning("Error : Please select an object")
         return
     # get the selected object's material and if not material prompt user to assign a material#
     theNodes = cmds.ls(sl = True, dag = True, s = True)
@@ -17,27 +18,34 @@ def convert_diffuse_material() :
     arnold_mat = cmds.ls(cmds.listConnections(shadeEng), materials = True)
     if cmds.nodeType(arnold_mat) != 'aiStandardSurface':
         cmds.confirmDialog(title='Error', message='Selected object does not have an Arnold Standard Surface material applied.', button=['OK'], defaultButton='OK')
-        cmds.warning("Selected object does not have an Arnold Standard Surface material applied.")
+        cmds.warning("Error : Selected object does not have an Arnold Standard Surface material applied.")
         return
     print(arnold_mat)
-    
-    # create a renderman shader and connect the arnold material attributes to the renderman shader attributes#
-    renderman_shader = cmds.shadingNode('PxrDiffuse', asShader=True)
-    cmds.rename(renderman_shader, 'rendermanDiffuse')
+
+    # Create a new RenderMan PxrSurface material
+    renderman_shader = cmds.shadingNode('PxrSurface', asShader=True)
     shading_group = cmds.sets(renderman_shader, renderable=True, noSurfaceShader=True, empty=True, name=renderman_shader + 'SG')
     cmds.connectAttr(renderman_shader + '.outColor', shading_group + '.surfaceShader', force=True)
+    renderman_attributes=cmds.listAttr(renderman_shader)
+    print(renderman_attributes)
 
-    arnold_color = cmds.getAttr(arnold_mat + '.baseColor')[0]
-    cmds.setAttr(renderman_shader + '.diffuseColor', arnold_color[0], arnold_color[1], arnold_color[2], type='float3')
+    #components_arnold_og= ['message', 'caching', 'frozen', 'isHistoricallyInteresting', 'nodeState', 'binMembership', 'outColor', 'outColorR', 'outColorG', 'outColorB', 'outAlpha', 'outTransparency', 'outTransparencyR', 'outTransparencyG', 'outTransparencyB', 'normalCamera', 'normalCameraX', 'normalCameraY', 'normalCameraZ', 'aiEnableMatte', 'aiMatteColor', 'aiMatteColorR', 'aiMatteColorG', 'aiMatteColorB', 'aiMatteColorA', 'base', '.baseColor', 'baseColorR', 'baseColorG', 'baseColorB', 'diffuseRoughness', 'specular', 'specularColor', 'specularColorR', 'specularColorG', 'specularColorB', 'specularRoughness', 'specularIOR', 'specularAnisotropy', 'specularRotation', 'metalness', 'transmission', 'transmissionColor', 'transmissionColorR', 'transmissionColorG', 'transmissionColorB', 'transmissionDepth', 'transmissionScatter', 'transmissionScatterR', 'transmissionScatterG', 'transmissionScatterB', 'transmissionScatterAnisotropy', 'transmissionDispersion', 'transmissionExtraRoughness', 'transmitAovs', 'subsurface', 'subsurfaceColor', 'subsurfaceColorR', 'subsurfaceColorG', 'subsurfaceColorB', 'subsurfaceRadius', 'subsurfaceRadiusR', 'subsurfaceRadiusG', 'subsurfaceRadiusB', 'subsurfaceScale', 'subsurfaceAnisotropy', 'subsurfaceType', 'sheen', 'sheenColor', 'sheenColorR', 'sheenColorG', 'sheenColorB', 'sheenRoughness', 'thinWalled', 'tangent', 'tangentX', 'tangentY', 'tangentZ', 'coat', 'coatColor', 'coatColorR', 'coatColorG', 'coatColorB', 'coatRoughness', 'coatIOR', 'coatAnisotropy', 'coatRotation', 'coatNormal', 'coatNormalX', 'coatNormalY', 'coatNormalZ', 'thinFilmThickness', 'thinFilmIOR', 'emission', 'emissionColor', 'emissionColorR', 'emissionColorG', 'emissionColorB', 'opacity', 'opacityR', 'opacityG', 'opacityB', 'caustics', 'internalReflections', 'exitToBackground', 'indirectDiffuse', 'indirectSpecular', 'dielectricPriority', 'aovId1', 'id1', 'id1R', 'id1G', 'id1B', 'aovId2', 'id2', 'id2R', 'id2G', 'id2B', 'aovId3', 'id3', 'id3R', 'id3G', 'id3B', 'aovId4', 'id4', 'id4R', 'id4G', 'id4B', 'aovId5', 'id5', 'id5R', 'id5G', 'id5B', 'aovId6', 'id6', 'id6R', 'id6G', 'id6B', 'aovId7', 'id7', 'id7R', 'id7G', 'id7B', 'aovId8', 'id8', 'id8R', 'id8G', 'id8B']
+    components_arnold= ['.baseColor']
+    print(components_arnold)
+    #components_renderman_og = ['message', 'caching', 'frozen', 'isHistoricallyInteresting', 'nodeState', 'binMembership', 'inputMaterial', 'diffuseGain', 'diffuseColor', 'diffuseColorR', 'diffuseColorG', 'diffuseColorB', 'diffuseRoughness', 'diffuseExponent', 'diffuseBumpNormal', 'diffuseBumpNormalX', 'diffuseBumpNormalY', 'diffuseBumpNormalZ', 'diffuseDoubleSided', 'diffuseBackUseDiffuseColor', 'diffuseBackColor', 'diffuseBackColorR', 'diffuseBackColorG', 'diffuseBackColorB', 'diffuseTransmitGain', 'diffuseTransmitColor', 'diffuseTransmitColorR', 'diffuseTransmitColorG', 'diffuseTransmitColorB', 'specularFresnelMode', 'specularFaceColor', 'specularFaceColorR', 'specularFaceColorG', 'specularFaceColorB', 'specularEdgeColor', 'specularEdgeColorR', 'specularEdgeColorG', 'specularEdgeColorB', 'specularFresnelShape', 'specularIor', 'specularIorR', 'specularIorG', 'specularIorB', 'specularExtinctionCoeff', 'specularExtinctionCoeffR', 'specularExtinctionCoeffG', 'specularExtinctionCoeffB', 'specularRoughness', 'specularModelType', 'specularAnisotropy', 'specularAnisotropyDirection', 'specularAnisotropyDirectionX', 'specularAnisotropyDirectionY', 'specularAnisotropyDirectionZ', 'specularBumpNormal', 'specularBumpNormalX', 'specularBumpNormalY', 'specularBumpNormalZ', 'specularDoubleSided', 'roughSpecularFresnelMode', 'roughSpecularFaceColor', 'roughSpecularFaceColorR', 'roughSpecularFaceColorG', 'roughSpecularFaceColorB', 'roughSpecularEdgeColor', 'roughSpecularEdgeColorR', 'roughSpecularEdgeColorG', 'roughSpecularEdgeColorB', 'roughSpecularFresnelShape', 'roughSpecularIor', 'roughSpecularIorR', 'roughSpecularIorG', 'roughSpecularIorB', 'roughSpecularExtinctionCoeff', 'roughSpecularExtinctionCoeffR', 'roughSpecularExtinctionCoeffG', 'roughSpecularExtinctionCoeffB', 'roughSpecularRoughness', 'roughSpecularModelType', 'roughSpecularAnisotropy', 'roughSpecularAnisotropyDirection', 'roughSpecularAnisotropyDirectionX', 'roughSpecularAnisotropyDirectionY', 'roughSpecularAnisotropyDirectionZ', 'roughSpecularBumpNormal', 'roughSpecularBumpNormalX', 'roughSpecularBumpNormalY', 'roughSpecularBumpNormalZ', 'roughSpecularDoubleSided', 'clearcoatFresnelMode', 'clearcoatFaceColor', 'clearcoatFaceColorR', 'clearcoatFaceColorG', 'clearcoatFaceColorB', 'clearcoatEdgeColor', 'clearcoatEdgeColorR', 'clearcoatEdgeColorG', 'clearcoatEdgeColorB', 'clearcoatFresnelShape', 'clearcoatIor', 'clearcoatIorR', 'clearcoatIorG', 'clearcoatIorB', 'clearcoatExtinctionCoeff', 'clearcoatExtinctionCoeffR', 'clearcoatExtinctionCoeffG', 'clearcoatExtinctionCoeffB', 'clearcoatThickness', 'clearcoatAbsorptionTint', 'clearcoatAbsorptionTintR', 'clearcoatAbsorptionTintG', 'clearcoatAbsorptionTintB', 'clearcoatRoughness', 'clearcoatModelType', 'clearcoatAnisotropy', 'clearcoatAnisotropyDirection', 'clearcoatAnisotropyDirectionX', 'clearcoatAnisotropyDirectionY', 'clearcoatAnisotropyDirectionZ', 'clearcoatBumpNormal', 'clearcoatBumpNormalX', 'clearcoatBumpNormalY', 'clearcoatBumpNormalZ', 'clearcoatDoubleSided', 'specularEnergyCompensation', 'clearcoatEnergyCompensation', 'iridescenceFaceGain', 'iridescenceEdgeGain', 'iridescenceFresnelShape', 'iridescenceMode', 'iridescencePrimaryColor', 'iridescencePrimaryColorR', 'iridescencePrimaryColorG', 'iridescencePrimaryColorB', 'iridescenceSecondaryColor', 'iridescenceSecondaryColorR', 'iridescenceSecondaryColorG', 'iridescenceSecondaryColorB', 'iridescenceRoughness', 'iridescenceAnisotropy', 'iridescenceAnisotropyDirection', 'iridescenceAnisotropyDirectionX', 'iridescenceAnisotropyDirectionY', 'iridescenceAnisotropyDirectionZ', 'iridescenceBumpNormal', 'iridescenceBumpNormalX', 'iridescenceBumpNormalY', 'iridescenceBumpNormalZ', 'iridescenceCurve', 'iridescenceScale', 'iridescenceFlip', 'iridescenceThickness', 'iridescenceDoubleSided', 'fuzzGain', 'fuzzColor', 'fuzzColorR', 'fuzzColorG', 'fuzzColorB', 'fuzzConeAngle', 'fuzzBumpNormal', 'fuzzBumpNormalX', 'fuzzBumpNormalY', 'fuzzBumpNormalZ', 'fuzzDoubleSided', 'subsurfaceType', 'subsurfaceGain', 'subsurfaceColor', 'subsurfaceColorR', 'subsurfaceColorG', 'subsurfaceColorB', 'subsurfaceDmfp', 'subsurfaceDmfpColor', 'subsurfaceDmfpColorR', 'subsurfaceDmfpColorG', 'subsurfaceDmfpColorB', 'shortSubsurfaceGain', 'shortSubsurfaceColor', 'shortSubsurfaceColorR', 'shortSubsurfaceColorG', 'shortSubsurfaceColorB', 'shortSubsurfaceDmfp', 'longSubsurfaceGain', 'longSubsurfaceColor', 'longSubsurfaceColorR', 'longSubsurfaceColorG', 'longSubsurfaceColorB', 'longSubsurfaceDmfp', 'subsurfaceDirectionality', 'subsurfaceBleed', 'subsurfaceDiffuseBlend', 'subsurfaceResolveSelfIntersections', 'subsurfaceIor', 'subsurfacePostTint', 'subsurfacePostTintR', 'subsurfacePostTintG', 'subsurfacePostTintB', 'subsurfaceDiffuseSwitch', 'subsurfaceDoubleSided', 'subsurfaceTransmitGain', 'considerBackside', 'continuationRayMode', 'maxContinuationHits', 'followTopology', 'subsurfaceSubset', 'singlescatterGain', 'singlescatterColor', 'singlescatterColorR', 'singlescatterColorG', 'singlescatterColorB', 'singlescatterMfp', 'singlescatterMfpColor', 'singlescatterMfpColorR', 'singlescatterMfpColorG', 'singlescatterMfpColorB', 'singlescatterDirectionality', 'singlescatterIor', 'singlescatterBlur', 'singlescatterDirectGain', 'singlescatterDirectGainTint', 'singlescatterDirectGainTintR', 'singlescatterDirectGainTintG', 'singlescatterDirectGainTintB', 'singlescatterDoubleSided', 'singlescatterConsiderBackside', 'singlescatterContinuationRayMode', 'singlescatterMaxContinuationHits', 'singlescatterDirectGainMode', 'singlescatterSubset', 'irradianceTint', 'irradianceTintR', 'irradianceTintG', 'irradianceTintB', 'irradianceRoughness', 'unitLength', 'refractionGain', 'reflectionGain', 'refractionColor', 'refractionColorR', 'refractionColorG', 'refractionColorB', 'glassRoughness', 'glassRefractionRoughness', 'glassRefraction2Roughness', 'glassRefraction2Blend', 'glassRefraction2Tint', 'glassRefraction2TintR', 'glassRefraction2TintG', 'glassRefraction2TintB', 'glassAnisotropy', 'glassAnisotropyDirection', 'glassAnisotropyDirectionX', 'glassAnisotropyDirectionY', 'glassAnisotropyDirectionZ', 'glassBumpNormal', 'glassBumpNormalX', 'glassBumpNormalY', 'glassBumpNormalZ', 'glassIor', 'mwWalkable', 'mwIor', 'thinGlass', 'ignoreFresnel', 'ignoreAccumOpacity', 'blocksVolumes', 'volumeAggregate', 'volumeAggregateName', 'ssAlbedo', 'ssAlbedoR', 'ssAlbedoG', 'ssAlbedoB', 'extinction', 'extinctionR', 'extinctionG', 'extinctionB', 'g', 'g1', 'blend', 'volumeGlow', 'volumeGlowR', 'volumeGlowG', 'volumeGlowB', 'maxExtinction', 'multiScatter', 'enableOverlappingVolumes', 'glowGain', 'glowColor', 'glowColorR', 'glowColorG', 'glowColorB', 'bumpNormal', 'bumpNormalX', 'bumpNormalY', 'bumpNormalZ', 'shadowBumpTerminator', 'shadowColor', 'shadowColorR', 'shadowColorG', 'shadowColorB', 'shadowMode', 'presence', 'presenceCached', 'mwStartable', 'roughnessMollificationClamp', 'userColor', 'userColorR', 'userColorG', 'userColorB', 'utilityPattern', 'outColor', 'outColorR', 'outColorG', 'outColorB', 'outGlowColor', 'outGlowColorR', 'outGlowColorG', 'outGlowColorB', 'outMatteOpacity', 'outMatteOpacityR', 'outMatteOpacityG', 'outMatteOpacityB', 'outTransparency', 'outTransparencyR', 'outTransparencyG', 'outTransparencyB', 'attributeAliasList']
+    components_renderman = ['.diffuseColor']
+    print(components_renderman)
 
-    #assign the renderman_shader to the selected object#
-    cmds.select(sel[0])
-    cmds.hyperShade(assign=renderman_shader)
-
-    print("Arnold Standard Surface material successfully converted to RenderMan PxRDiffuse.")
+    for attr in components_arnold :
+        arnold_mat=arnold_mat+attr
+        value=cmds.getAttr(arnold_mat)
+        file_node=cmds.connectionInfo(arnold_mat, sourceFromDestination=True)
+        if attr == '.baseColor' :
+            renderman_shader.append('.diffuseColor')
+            cmds.setAttr(renderman_shader,value[0][0],value[0][1],value[0][2],type='double3')
+            if file_node :
+                file_node.append()
+                file_path = cmds.getAttr(file_node + '.fileTextureName')
+                cmds.setAttr(renderman_shader+'.diffuseColorMap', file_path, type='string')
 
 convert_diffuse_material()
-    
-
-
-
